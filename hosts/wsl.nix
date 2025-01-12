04ef71b2343677ed2286a327d87d9f782ab4c10b
Vu Nhat Chuong <ronnyvu321@gmail.com>
2025-01-12 15:37:01 +0700

    feat: wsl

--- hosts/wsl.nix
+++ hosts/wsl.nix
@@ -1,19 +1,21 @@
 { pkgs, currentSystemUser, ... }: {
-  imports = [];
+  imports = [
+    ../modules/common.nix
+    ../modules/net.nix
+    ../modules/nh.nix
+    ../modules/nix.nix
+    ../modules/packages/common.nix
+  ];
+
+  system.stateVersion = "24.11";
 
   wsl = {
     enable = true;
     wslConf.automount.root = "/mnt";
-    defaultUser = currentSystemUser;
+    defaultUser = "ronny";
     startMenuLaunchers = true;
-  };
 
-  nix = {
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-      keep-outputs = true
-      keep-derivations = true
-    '';
+    # Enable integration with Docker Desktop (needs to be installed)
+    docker-desktop.enable = false;
   };
 }
